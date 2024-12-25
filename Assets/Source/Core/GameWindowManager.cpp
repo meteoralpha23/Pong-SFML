46fa3cd5b459c3ce7d20cb656da5080761ddc1b9
meteoralpha23 <luckycreed123@gmail.com>
2024-12-25 10:59:44 +0530

    Fetching mouse coordinates

--- Assets/Source/Core/GameWindowManager.cpp
+++ Assets/Source/Core/GameWindowManager.cpp
@@ -1,37 +1,39 @@
 #include "../../Header/Core/GameWindowManager.h"
 
 
-
-void Core::GameWindowManager::initialize()
+namespace Core
 {
-	game_window = new RenderWindow();
-	createGameWindow();
+	void Core::GameWindowManager::initialize()
+	{
+		game_window = new RenderWindow();
+		createGameWindow();
+	}
+
+
+
+	void Core::GameWindowManager::createGameWindow()
+	{
+
+		game_window->create(sf::VideoMode::getDesktopMode(),
+			game_title, sf::Style::Fullscreen);
+
+	}
+
+	bool Core::GameWindowManager::isGameRunning()
+	{
+		return game_window->isOpen();
+	}
+
+	void Core::GameWindowManager::render()
+	{
+		game_window->clear(sf::Color(200, 50, 50, 255));
+
+		game_window->display();
+
+	}
+	RenderWindow* Core::GameWindowManager::getGameWindow()
+	{
+		return game_window;
+	}
+
 }
-
-
-
-void Core::GameWindowManager::createGameWindow()
-{
-
-	game_window->create(sf::VideoMode::getDesktopMode(),
-		game_title,sf::Style::Fullscreen);
-
-}
-
-bool Core::GameWindowManager::isGameRunning()
-{
-	return game_window->isOpen();
-}
-
-void Core::GameWindowManager::render()
-{
-	game_window->clear(sf::Color(200, 50, 50, 255));
-
-	game_window->display();
-
-}
-RenderWindow* Core::GameWindowManager::getGameWindow()
-{
-	return game_window;
-}
-
