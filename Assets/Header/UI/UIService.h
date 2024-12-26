473e067a362a8cf751ba3f3cffb78eba09d8cd31
meteoralpha23 <luckycreed123@gmail.com>
2024-12-26 17:36:27 +0530

    score2

--- Assets/Header/UI/UIService.h
+++ Assets/Header/UI/UIService.h
@@ -10,37 +10,39 @@ namespace UI
 	class UIService
 	{
 	protected:
-		Font font;
-		Text left_score_text;
+        Font font;
+        Text left_score_text;
+        Text right_score_text;
 
-		string texture_path = "Assets/Fonts/Aloevera-OVoWO.ttf";
+        string texture_path = "Assets/Fonts/Aloevera-OVoWO.ttf";
 
-		int font_size = 40;
-		Color font_color = Color::White;
+        int font_size = 40;
+        Color font_color = Color::White;
+        string initial_string = "00";
 
-		string initial_string = "00";
+        float left_score_postion_x = 570.0f;
+        float left_score_postion_y = 30.0f;
 
-		float left_score_position_x = 570.0f;
-		float left_score_position_y = 30.0f;
+        float right_score_position_x = 670.0f;
+        float right_score_position_y = 30.0f;
 
-		float right_score_position_x = 670.0f;
-		float right_score_position_y = 30.0f;
+        int player1_score = 0;
+        int player2_score = 0;
 
-		int player1_score = 0;
+        string formatScore(int score);
 
-
-		void loadFontTexture();
-		void createLeftScoreText();
-		void createRightScoreText();
+        void loadFontTexture();
+        void createLeftScoreText();
+        void createRightScoreText();
 
 	public:
-		UIService();
+        UIService();
 
-		void incrementPlayer1Score();
-		void incrementPlayer2Score();
+        void incrementPlayer1Score();
+        void incrementPlayer2Score();
 
-		void update();
-		void render(RenderWindow* game_window);
+        void update();
+        void render(RenderWindow* game_window);
 
 	};
 }
